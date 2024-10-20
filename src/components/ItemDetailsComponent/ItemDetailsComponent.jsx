12f19fd79ad0a36f7065672c6d735056300407e7
Vasanthkumar Sadasivam <vasanth37cbe@gmail.com>
2024-10-20 12:26:35 +0530

    Adding Delete function

--- src/components/ItemDetailsComponent/ItemDetailsComponent.jsx
+++ src/components/ItemDetailsComponent/ItemDetailsComponent.jsx
@@ -3,27 +3,51 @@ import React from "react";
 import BidDetailsComponent from "./BidDetailsComponent";
 import DescriptionComponent from "./DescriptionComponent";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Checkbox, Typography } from "@mui/material";
+import { Checkbox, Typography } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import ButtonComponent from "../ContentComponent/ButtonComponent";
 import { useNavigate } from "react-router-dom";
-import { updateSeletedItemDetails } from "../../Reducer/biddingAppSlice";
+import {
+  updateBidPopupState,
+  updateSeletedItemDetails,
+} from "../../Reducer/biddingAppSlice";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import DummyImage from "../../assets/Icons/DummyImage.png";
 import { Favorite, FavoriteBorder } from "@mui/icons-material";
 import CardContent from "../CardComponent/CardContent";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import BidDialogComponent from "../BidDialogComponent/BidDialogComponent";
+import axios from "axios";
 
 function ItemDetailsComponent() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const itemDetails = useSelector((state) => state.biddingApp.selectedItem);
+  const openBidPopup = useSelector((state) => state.biddingApp.openBidPopup);
 
   const backToCatalogClickHandler = () => {
     dispatch(updateSeletedItemDetails({}));
     navigate("/");
+  };
+
+  const bidNowClickHandler = () => {
+    dispatch(updateBidPopupState(true));
+  };
+
+  const deleteItemClickHandler = () => {
+    let config = {
+      method: "delete",
+      maxBodyLength: Infinity,
+      url: `http://localhost:5555/items/${itemDetails?._id}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    axios.request(config).then((response) => {
+      navigate("/")
+    });
   };
 
   return (
@@ -52,21 +76,31 @@ function ItemDetailsComponent() {
           </Grid>
           <Grid size={12} display="flex">
             <ButtonComponent startIcon={<EditIcon />}>Edit</ButtonComponent>
-            <ButtonComponent startIcon={<DeleteIcon />}>Delete</ButtonComponent>
+            <ButtonComponent
+              startIcon={<DeleteIcon />}
+              onClick={deleteItemClickHandler}
+            >
+              Delete
+            </ButtonComponent>
           </Grid>
         </Grid>
       </Grid>
       <Grid size={8}>
         <Typography>Description</Typography>
+        <Typography>
+          {itemDetails?.description ?? "No description available"}
+        </Typography>
       </Grid>
       <Grid size={2}>
         <ButtonComponent
           endIcon={<KeyboardArrowRightIcon />}
           variant="contained"
+          onClick={bidNowClickHandler}
         >
           Bid now
         </ButtonComponent>
       </Grid>
+      {openBidPopup && <BidDialogComponent />}
     </Grid>
   );
 }
