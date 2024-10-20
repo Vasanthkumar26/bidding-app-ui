4a5f30d79adfd2d0caba49b35fd708569aab620a
Vasanthkumar Sadasivam <vasanthkumar.sadasivam@hitachienergy.com>
2024-10-20 11:12:26 +0530

    Making changes on Card Component

--- src/components/CardComponent/CardHeader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CardHeader(props) {
-  return (
-    <img src="" alt="image-of-article"/>
-  )
-}
-
-export default React.memo(CardHeader);
--- src/components/ItemDetailsComponent/ItemDetailsComponent.jsx
+++ src/components/ItemDetailsComponent/ItemDetailsComponent.jsx
@@ -2,14 +2,18 @@ import Grid from "@mui/material/Grid2";
 import React from "react";
 import BidDetailsComponent from "./BidDetailsComponent";
 import DescriptionComponent from "./DescriptionComponent";
-import CardContent from "../CardComponent/CardContent";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Typography } from "@mui/material";
+import { Button, Checkbox, Typography } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import ButtonComponent from "../ContentComponent/ButtonComponent";
 import { useNavigate } from "react-router-dom";
 import { updateSeletedItemDetails } from "../../Reducer/biddingAppSlice";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import DummyImage from "../../assets/Icons/DummyImage.png";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import CardContent from "../CardComponent/CardContent";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function ItemDetailsComponent() {
   const navigate = useNavigate();
@@ -24,20 +28,44 @@ function ItemDetailsComponent() {
 
   return (
     <Grid sx={{ padding: "1rem 3rem 0rem 3rem" }} container spacing={2}>
-      <Grid size={2}>
+      <Grid size={2} display="grid">
         <ButtonComponent
           startIcon={<KeyboardArrowLeftIcon />}
           onClick={backToCatalogClickHandler}
         >
           Back to catalog
         </ButtonComponent>
-        <CardContent item={itemDetails} />
+        <Grid sixe={12} spacing={2} container>
+          <Grid size={12} display="flex">
+            <img alt="image-of-article" src={DummyImage} width="100%" />
+            <Grid>
+              <Checkbox
+                sx={{ position: "relative", right: "2rem" }}
+                icon={<FavoriteBorder />}
+                checkedIcon={<Favorite sx={{ color: "red" }} />}
+                checked={itemDetails.isFavourite}
+              />
+            </Grid>
+          </Grid>
+          <Grid size={12}>
+            <CardContent item={itemDetails} />
+          </Grid>
+          <Grid size={12} display="flex">
+            <ButtonComponent startIcon={<EditIcon />}>Edit</ButtonComponent>
+            <ButtonComponent startIcon={<DeleteIcon />}>Delete</ButtonComponent>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid size={8}>
         <Typography>Description</Typography>
       </Grid>
       <Grid size={2}>
-        <ButtonComponent endIcon={<KeyboardArrowRightIcon />} variant="contained">Bid now</ButtonComponent>
+        <ButtonComponent
+          endIcon={<KeyboardArrowRightIcon />}
+          variant="contained"
+        >
+          Bid now
+        </ButtonComponent>
       </Grid>
     </Grid>
   );
